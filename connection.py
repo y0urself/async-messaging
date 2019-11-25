660b40a9653638fa32e5786d8388b5631a2098cb
jstach <jstach@uos.de>
2019-11-25 16:44:46 +0100

    Removed not working Class, added simple pika RPC component to answer remote requests over AMQP

--- connection.py
+++ connection.py
@@ -5,43 +5,47 @@ import pika
 import argparse
 
  
-class AmqpHelper():
-    """
-    For the asynchronous pika-messaging over rabbitmq
-    """
-    def __init__(self):
-        self.param = None
-        self.conn = None
-        self.chan = None
+# class AmqpHelper():
+#     """
+#     For the asynchronous pika-messaging over rabbitmq
+#     """
+#     def __init__(self):
+#         self.param = None
+#         self.conn = None
+#         self.chan = None
 
-    def channel(self):
-        if self.conn:
-            print("creating channel")
-            self.chan = self.conn.channel(on_open_callback= self.exchange)
-        else:
-            print("no channel created ...")
+#     def connect(self, param = pika.URLParameters('amqp://guest:guest@localhost:5672/%2F')):
+#         self.param = param
+#         self.conn = pika.SelectConnection(parameters = param, on_open_callback=self.channel)
 
-    def exchange(self, exchange):
-        if self.chan:
-            print("declare exchange")
-            self.chan.exchange_declare(exchange=exchange, callback=self.queue)
+#     def channel(self):
+#         if self.conn:
+#             print("creating channel")
+#             self.chan = self.conn.channel(on_open_callback= self.exchange)
+#         else:
+#             print("no channel created ...")
 
-    def queue(self, queue):
-        if self.chan:
-            print("declare queue")
-            self.chan.queue_declare(queue=queue)
+#     def exchange(self, exchange):
+#         if self.chan:
+#             print("declare exchange")
+#             self.chan.exchange_declare(exchange=exchange, callback=self.queue)
 
-    def publish(self):
-        if self.chan:
-            self.chan.basic_publish('test_', 'lol-plz', 'i_am_a_message_...')
-        else:
-            print("basically not published")
+#     def queue(self, queue):
+#         if self.chan:
+#             print("declare queue")
+#             self.chan.queue_declare(queue=queue)
 
-    def start(self):
-        if self.conn and not self.conn.is_open:
-            self.conn.ioloop.start()                #now we are lost in the loop ...! maybe thread it out?
-        else:
-            print("not started")
+#     def publish(self):
+#         if self.chan:
+#             self.chan.basic_publish('test_', 'lol-plz', 'i_am_a_message_...')
+#         else:
+#             print("basically not published")
+
+#     def start(self):
+#         if self.conn and not self.conn.is_open:
+#             self.conn.ioloop.start()                #now we are lost in the loop ...! maybe thread it out?
+#         else:
+#             print("not started")
             
 
 class ConnDummy():
@@ -57,7 +61,7 @@ class ConnDummy():
         self.stdout = None
         self.stderr = None
 
-        self.amqp = None
+        #self.amqp = None
 
     def parse(self):
         self.parser.add_argument('-p', '--port', dest='port', default=22, type=int, help='Port of the host')
@@ -106,15 +110,32 @@ class ConnDummy():
             print(response)
             return False
 
-    def setup(self):
-        self.amqp = AmqpHelper()
+    def get(self, request):
+        self.stdout.channel.send(request)
 
-        # scheme://username:password@host:port/virtual_host?key=value&key=value
-        self.amqp.param = pika.URLParameters('amqp://guest:guest@localhost:5672/%2F')
-        self.amqp.conn = pika.SelectConnection(parameters = self.amqp.param, on_open_callback=self.amqp.channel)
+        return self.stdin.channel.recv(150000)
+
+
+conn = ConnDummy()
+
+def on_request(ch, method, props, body):
+    print(" Request received {}".format(body))
+    #print("Lets open a File ... having a good time ...")
+    #with open("lol.txt", 'w') as f:
+    #    f.write("lol")
+    #print("lol!")
+    response = conn.get(body)
+
+    properties = pika.BasicProperties(correlation_id = props.correlation_id)
+
+    ch.basic_publish(exchange='', 
+                     routing_key=props.reply_to, 
+                     properties=properties,
+                     body=str(response)
+                     )
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 def main():
-    conn = ConnDummy()
     conn.parse()
 
     if not conn.connect():
@@ -123,20 +144,23 @@ def main():
     if not conn.authenticate():
         exit(1)
 
-    conn.setup()
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='gvm_requests')
+
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue='gvm_requests', on_message_callback=on_request)
 
     #TODO We want to wait for requests here, 
     # that we can pass over to the GMP
     try:
-        print("connecting pika pika")
-        conn.amqp.start()
-    except KeyboardInterrupt:
-        print("is this .... " + str(conn.amqp.conn.is_open))
-        print("catched ...")
-        #conn.amqp.conn.close() # the tut says close but the reality says ConnectionWrongStateError. ^^
+        channel.start_consuming()
 
-        # Start the IOLoop again so Pika can communicate, it will stop on its own when the connection is closed
         #conn.amqp.start()
+    except KeyboardInterrupt:
+        connection.close()
 
 if __name__ == '__main__':
     main()
